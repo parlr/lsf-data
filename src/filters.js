78294665490a62a072ffec0e857d73cc0ec0ff5d
Edouard Lopez <contact@edouard-lopez.com>
2017-12-16 00:37:02 +0100

    remove useless

--- src/filters.js
+++
@@ -1,8 +0,0 @@
-export { highlight };
-
-function highlight(content, query) {
-  var search = new RegExp(query, 'ig');
-  return content
-    .toString()
-    .replace(search, match => `<span class="highlight">${match}</span>`);
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import "./assets/favicon.ico";
-import "./assets/manifest.json";
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
