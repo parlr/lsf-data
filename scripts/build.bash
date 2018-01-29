060e3c8f4875f4c0663719f800ffd7d6e43191dd
Edouard Lopez <contact@edouard-lopez.com>
2018-01-30 00:45:43 +0100

    import script and use them

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-source ./raw-to-production.bash
-source ./split.bash
-
+source ./scripts/split.bash ./"raw/partie 1"*.hd.mkv
 extract_timing_from_subtitles
 split_video
+
+source ./scripts/split.bash ./"raw/partie 2"*.hd.mkv
+extract_timing_from_subtitles
+split_video
+
+source ./raw-to-production.bash
